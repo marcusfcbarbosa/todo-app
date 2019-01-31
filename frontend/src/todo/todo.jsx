608ee122b41c6c9cad371bc745222629c57d0581
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-01-31 16:38:15 -0200

    Criando dois componentes base de formulario e listagem e os inserindo na pagina principal

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,5 +1,8 @@
 import React , {Component} from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
 
 //Componente por classe
 export default class Todo extends Component{
@@ -7,6 +10,8 @@ export default class Todo extends Component{
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+                <TodoForm/>
+                <TodoList />
             </div>
         )
     }
