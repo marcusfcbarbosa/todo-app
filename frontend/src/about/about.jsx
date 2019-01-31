4f0a3cd22465d8347499cd69956a2b9c3c152f81
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-01-31 15:24:42 -0200

    Adicionando dois componentes dentro de main app.jsx Todo e About

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,7 @@
+import React  from 'react'
+//Componente por classe
+export default props =>(
+ <div>
+     <h1>Sobre</h1>
+ </div>   
+)
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React , {Component} from 'react'
+//Componente por classe
+export default class Todo extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
