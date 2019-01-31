43f0ef115c8180b672b03b1f530ffa4f5402422f
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-01-31 16:08:41 -0200

    criando um componente PageHeader passando parametros para dentro do mesmo e importando ele em Todo.jsx

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,12 @@
 import React , {Component} from 'react'
+import PageHeader from '../template/pageHeader'
+
 //Componente por classe
 export default class Todo extends Component{
     render(){
         return(
             <div>
-                <h1>Todo</h1>
+                <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
             </div>
         )
     }
