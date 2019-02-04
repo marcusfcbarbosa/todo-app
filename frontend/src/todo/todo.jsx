79b7f5bbfc0e0f05136350831dcfa6f7938129ac
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-02-04 13:41:57 -0200

    Atribuindo eventos de clique e onChange de campos de input e trabalhando com o estado do componente

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -8,19 +8,31 @@ export default class Todo extends Component{
 
     constructor(props){
         super(props)
+            this.state = { description:'', list:[] }
+
             this.handleAdd = this.handleAdd.bind(this)//Dessa forma o this, irá esta vinculado ao contexto de quem o chama
+            this.handleChange = this.handleChange.bind(this)
     }
     
     //a principio o this esta null
     handleAdd(){
-        console.log(this);
+        console.log(this.state.description);
+    }
+
+    handleChange(event){//evento
+            this.setState({ ...this.state, description: event.target.value })
+            //pega, clona o estado (que contém a description e a list e atribui a description ao event.taget.value)
     }
 
     render(){
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm handleAdd ={this.handleAdd}/>
+                <TodoForm
+                    description = { this.state.description }
+                    handleAdd ={this.handleAdd}
+                    handleChange={this.handleChange}
+                />
                 <TodoList />
             </div>
         )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,7 +6,10 @@ import IconButton from '../template/iconButton'
 export default props =>(
     <div role="form" className="todoForm">
         <Grid cols='12 9 10'>
-            <input id="description" className="form-control"
+            <input id="description" 
+                value={ props.description }
+                onChange={props.handleChange}
+                className="form-control"
                 placeholder="Adicione uma tarefa">
             </input>   
         </Grid>
