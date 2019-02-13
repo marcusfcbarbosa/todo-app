5fac8cd08344c6d00d18ca7fc9df979ba941c049
marcus fernando <marcusfcbarbosa@gmail.com>
2019-02-13 11:39:15 -0200

    limpando o Todo.jsx, ja que ele nao controla mais o estado das aplicações de seus componentes

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,5 +1,4 @@
 import React , {Component} from 'react'
-import axios from 'axios'//usado para requisições HTTP
 //axios é baseado em promisses, ou seja, é assíncrono
 
 import PageHeader from '../template/pageHeader'
@@ -8,23 +7,15 @@ import TodoList from './todoList'
 
 const URL = 'http://localhost:3003/api/todos'
 
-//Componente por classe
-export default class Todo extends Component {
-    
-
-
-    render(){
-        return(
-            <div>
+export default props =>(
+<div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
                 
                 <TodoForm/>
 
                 <TodoList/>
-            </div>
-        )
-    }
-}
-//Como foi conectado o reducer no componente TodoList, nao preciso mais passar o estado para dentro do
-//componente  (TodoList) list={this.state.list}
-//TodoForm description = { this.state.description }
+</div>
+
+)
+
+
